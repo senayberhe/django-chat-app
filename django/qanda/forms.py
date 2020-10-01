b96c77c3f9463cd10f8455103dc3b1bca6df8408
senay berhe <tsionberhe@gmail.com>
2020-09-30 20:55:34 -0700

    created views and templates and also url

--- django/qanda/forms.py
+++ django/qanda/forms.py
@@ -1,6 +1,6 @@
 from django import forms 
 from django.contrib.auth import get_user_model
-from qanda.models import Question 
+from qanda.models import Question, Answer
 
 class QuestionForm(forms.ModelForm):
     user = forms.ModelChoiceField(
@@ -11,4 +11,32 @@ class QuestionForm(forms.ModelForm):
 
     class Meta:
         model = Question
-        ffields = ['title', 'question', 'user',]
+        fields = ['title', 'question', 'user',]
+
+
+class AnswerForm(forms.ModelForm):
+    user = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=get_user_model().objects.all(),
+        disabled=True,
+    )
+    question = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=Question.objects.all(),
+        disabled=True,
+    )
+
+    class Meta:
+        model = Answer
+        fields = ['answer', 'user', 'question',]
+
+
+class AnswerAcceptanceForm(forms.ModelForm):
+    accepted = forms.BooleanField(
+        widget=forms.HiddenInput,
+        required=False,
+    )
+
+    class Meta:
+        model = Answer
+        fields = ['accepted',]
--- django/qanda/views.py
+++ django/qanda/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from djnago.views.generic import CreateView
+from django.views.generic import CreateView, DayArchiveView, RedirectView
 
-from qanda.forms import QuestionForm
-from qanda.models import Question
-from django.http import HttpResponseBadRequest
+from qanda.forms import QuestionForm, AnswerForm, AnswerAcceptanceForm
+from qanda.models import Question, Answer
+from django.http import HttpResponseBadRequest, HttpResponseRedirect
+from qanda.forms import AnswerForm, AnswerAcceptanceForm
 
-class AskQuestionView(LoginRequiredMixine):
+
+class AskQuestionView(LoginRequiredMixin):
     form_class = QuestionForm
     template_name = 'qanda/ask.html'
 
@@ -27,3 +29,93 @@ class AskQuestionView(LoginRequiredMixine):
             ctx = self.get_context_data(preview=preview)
             return self.render_to_response(context=ctx)
         return HttpResponseBadRequest()
+
+
+class QuestionDetailView(DetailView):
+    model = Question 
+
+    ACCEPT_FORM = AnswerAcceptanceForm(initial={'accepted': True})
+    REJECT_FORM = AnswerAcceptanceForm(initial={'accepted': False})
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx.update({
+            'answer_form': AnswerForm(initial={
+                'user': self.request.user.id,
+                'question': self.object.id,
+            })
+        })
+        if self.object.can_accept_answers(self.request.user):
+            ctx.update({
+                'accept_form': self.ACCEPT_FORM,
+                'reject_form': self.REJECT_FORM,
+            })
+        return ctx
+    
+
+class CreateAnswerView(LoginRequiredMixin, CreateView):
+    form_class = AnswerForm
+    template_name = 'qanda/create_answer.html'
+
+    def get_initial(self):
+        return {
+            'question': self.get_question().id,
+            'user': self.request.user.id,
+        }
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(question=self.get_question(),
+                        **kwargs)
+
+    def get_success_url(self):
+        return self.object.question.get_absolute_url()
+
+
+    def form_valid(self, form):
+        action = self.request.POST.get('action')
+        if action == 'SAVE':
+            #save and redirect as usual.
+            return super().form_valid(form)
+        elif action == 'PREVIEW':
+            ctx = self.get_context_data(preview=form.cleaned_data['answer'])
+            return self.render_to_response(context=ctx)
+        return HttpResponseBadRequest()
+
+    
+    def get_question(self):
+        return Question.objects.get(pk=self.kwargs['pk'])
+
+    
+
+class UpdateAnswerAcceptance(LoginRequiredMixin, UpdateView):
+    form_class = AnswerAcceptanceForm
+    queryset = Answer.objects.all()
+
+    def get_success_url(self):
+        return self.object.question.get_absolute_url()
+
+    def form_invalid(self, form):
+        return HttpResponseRedirect(
+            redirect_to=self.objects.question.get_absolute_url()
+        )
+
+
+class DailyQuestionList(DayArchiveView):
+    queryset = Question.objects.all()
+    date_field = 'created'
+    month_format = '%m'
+    allow_empty = True
+
+
+class TodayQuestionList(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        today = timezone.now()
+        return reverse(
+            'questions:daily_questions',
+            kwargs={
+                'day': today.day,
+                'month': today.month,
+                'year': today.year,
+            }
+        )
+    
